ea7ce962319bff3be4799acbad4476d7bc276c33
5se7en <177739007@qq.com>
2018-03-10 12:01:37 +0800

    add asyncData

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,19 +1,42 @@
 <template>
-  <section>
-    <my-Development/>
+  <section class="articleList">
+    <ul>
+    	<li v-for="item in articleList">{{ item.title }}</li>
+    </ul>
   </section>
 </template>
 
 <script>
-import MyDevelopment from '~/components/development/MyDevelopment.vue'
+import axios from 'axios';
 
 export default {
-  components: {
-    MyDevelopment
+  async asyncData ({ params }) {
+    let { data } = await axios.get(`/api/fortuneChur/hot/latest.html`)
+    return { articleList: data }
   }
-}
+}  
 </script>
 
 <style lang="scss" scoped>
-
+.articleList{
+  padding: 50px;
+  @media (max-width: 991px) {
+    padding-left: 0;
+    padding-right: 0;
+  }
+  ul{
+    li{
+	  width: 600px;
+	  margin: 0 auto;
+	  text-align: center;
+	  height: 50px;
+	  line-height: 50px;
+	  border-bottom: 1px solid #ccc;
+	  @media (max-width: 991px) {
+	  	width: 100%;
+	  	font-size: 14px;
+	  }
+	}
+  }
+}
 </style>
